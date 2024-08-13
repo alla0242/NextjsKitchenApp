3da615f4cb1705ad074ab8d5069e17c567b6476b
mat <alla0242@algonquinlive.com>
2024-08-13 19:19:58 -0400

    changed paths

--- src/app/pages/FoH/page.js
+++ src/app/pages/FoH/page.js
@@ -32,7 +32,7 @@ export default function FoH({ onOrderFinished }) {
   const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
   function fetchOrders() {
-    fetch(`${uri}/api/getLatestImages`)
+    fetch(`/api/getLatestImages`)
       .then((response) => response.json())
       .then((data) => {
         if (data.success && data.images) {
