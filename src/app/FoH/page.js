afff1d0eaca8cdacf59ba6e27009a202473f25f3
mat <alla0242@algonquinlive.com>
2024-08-13 16:55:22 -0400

    fucking fuck

--- src/app/FoH/page.js
+++ src/app/FoH/page.js
@@ -31,7 +31,7 @@ export default function FoH({ onOrderFinished }) {
   const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
   function fetchOrders() {
-    fetch(`https://kitchen-red.vercel.app/api/getLatestImages`)
+    fetch(`/api/getLatestImages`)
       .then((response) => response.json())
       .then((data) => {
         if (data.success && data.images) {
