1d8bb766de65de2a1c66feb3c0dc03814979d43b
mat <alla0242@algonquinlive.com>
2024-08-14 12:06:38 -0400

    test1

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -217,9 +217,7 @@ async function run() {
       }
     });
 
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
-    });
+
   } catch (error) {
     console.error(error);
   }
--- src/app/axios/test.js
+++ src/app/axios/test.js
@@ -1,3 +1,4 @@
+"use server"
 import { MongoClient } from "mongodb";
 async function run() {
   const uri =
