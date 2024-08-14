f1ca5c0e3ed820ac7f11749f88906c7d477bfe2e
mat <alla0242@algonquinlive.com>
2024-08-14 12:11:29 -0400

    hjkhjk

--- src/app/axios/page.js
+++ src/app/axios/page.js
@@ -1,43 +1,21 @@
-// import { MongoClient } from "mongodb";
-
-
-// async function run() {
-
-// const uri = process.env.MONGODB_URI;
-// const client = new MongoClient(uri);
-
-//   try {
-//     await client.connect();
-//     console.log("Connected successfully to MongoDB");
-//   } catch (error) {
-//     console.error("Failed to connect to MongoDB", error);
-//   }
-// }
-// const AxiosTest = () => {
-//   return (
-//     <div>
-//       <h1>Test</h1>
-//       <button onClick={_____}>Fetch One</button>
-//       FUUUUCK
-//     </div>
-//   );
-// };
-
-// export default AxiosTest;
 'use client';
 import run from "./test";
 
-
 function AxiosTest() {
- 
   return (
     <div>
       <h1>Test</h1>
-      <button onClick={run}>Fetch One</button>
+      <button onClick={async () => {
+        try {
+          const result = await run();
+          console.log(result);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      }}>Fetch One</button>
       FUUUUCK
     </div>
   );
 }
-
 
 export default AxiosTest;
