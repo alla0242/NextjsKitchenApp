90e4b726baf19b273aa0d0b82f4b0258bcfdd100
mat <alla0242@algonquinlive.com>
2024-08-14 10:40:10 -0400

    axios test

--- src/app/axios/page.js
+++ src/app/axios/page.js
@@ -1,53 +1,42 @@
 "use client";
 import axios from "axios";
-var data = JSON.stringify({
-  collection: "pastOrders",
-  database: "DrawingApp",
-  dataSource: "DrawingApp",
-  projection: {
-    _id: 1,
-    imageData: 1,
-    timestamp: 1,
-    state: 1,
-    lastChangeSource: 1,
-    lastChangeTime: 1,
-  },
-});
-var config = {
-  method: "post",
-  url: "https://data.mongodb-api.com/app/data-jywkgzt/endpoint/data/v1/action/findOne",
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Request-Headers": "*",
-    "api-key": "pGhXRQXyJuYVoVGCEKZ820en3nlE6h4NINZP1EfBGy8PMCLCNl9JYhTeYhbxilMj",
-  },
-  data: data,
-};
-axios(config)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+
 
 const AxiosTest = () => {
 
 
 
   function fetchOne() {
-    const data = JSON.stringify({
-      collection: "pastOrders",
-      database: "DrawingApp",
-      dataSource: "DrawingApp",
-      projection: {
-        _id: 1,
-        imageData: 1,
-        timestamp: 1,
-        state: 1,
-        lastChangeSource: 1,
-        lastChangeTime: 1,
-      },
+  var data = JSON.stringify({
+    collection: "pastOrders",
+    database: "DrawingApp",
+    dataSource: "DrawingApp",
+    projection: {
+      _id: 1,
+      imageData: 1,
+      timestamp: 1,
+      state: 1,
+      lastChangeSource: 1,
+      lastChangeTime: 1,
+    },
+  });
+  var config = {
+    method: "post",
+    url: "https://data.mongodb-api.com/app/data-jywkgzt/endpoint/data/v1/action/findOne",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key":
+        "pGhXRQXyJuYVoVGCEKZ820en3nlE6h4NINZP1EfBGy8PMCLCNl9JYhTeYhbxilMj",
+    },
+    data: data,
+  };
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
     });
   }
   return (
