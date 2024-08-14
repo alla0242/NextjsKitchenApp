d32ec70e9ac090cd4517d36e2914b375e56df351
mat <alla0242@algonquinlive.com>
2024-08-14 18:12:40 -0400

    added trycatch

--- src/app/BoH/setOrders.js
+++ src/app/BoH/setOrders.js
@@ -5,11 +5,21 @@ async function updateOrderState(orderId, newState) {
   const uri = process.env.MONGODB_URI;
   const client = new MongoClient(uri);
 
-  await client.connect();
-  await client
-    .db("DrawingApp")
-    .collection("images")
-    .updateOne({ _id: orderId }, { $set: { state: newState ,lastChangeTime: new Date(), lastChangeSource: "BoH"}});
+  try {
+    await client.connect();
+    await client
+      .db("DrawingApp")
+      .collection("images")
+      .updateOne(
+        { _id: orderId },
+        { $set: { state: newState, lastChangeTime: new Date(), lastChangeSource: "BoH" } }
+      );
+    console.log("Order updated");
+  } catch (error) {
+    console.error("Error updating order state:", error);
+  } finally {
+    await client.close();
+  }
 }
 
 export default updateOrderState;
