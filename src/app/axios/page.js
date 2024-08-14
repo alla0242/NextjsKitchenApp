52b84079826a8bf4bf68b8d8bbdd431ff3dcd5df
mat <alla0242@algonquinlive.com>
2024-08-14 11:52:09 -0400

    test9

--- src/app/axios/page.js
+++ src/app/axios/page.js
@@ -24,35 +24,13 @@
 // };
 
 // export default AxiosTest;
+'use client';
 const { MongoClient } = require("mongodb");
+import run from "./test";
 
 
 function AxiosTest() {
-  async function run() {
-    const uri =
-      "mongodb+srv://vercel-admin-user-66bcc8cb83ebb83e446851bb:hvBi8PHQ6sVd4Eeq@drawingapp.so9q8oz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-    const client = new MongoClient(uri);
-
-    try {
-      await client.connect();
-      console.log("Connected successfully to MongoDB");
-      await listDatabases(client);
-    } catch (error) {
-      console.error("Failed to connect to MongoDB", error);
-    } finally {
-      await client.close();
-      console.log("Closed connection to MongoDB");
-    }
-  }
-
-
-  async function listDatabases(client) {
-    databasesList = await client.db().admin().listDatabases();
-
-    console.log("Databases:");
-    databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
-  }
-
+ 
   return (
     <div>
       <h1>Test</h1>
---
+++ src/app/axios/test.js
@@ -0,0 +1,24 @@
+async function run() {
+  const uri =
+    "mongodb+srv://vercel-admin-user-66bcc8cb83ebb83e446851bb:hvBi8PHQ6sVd4Eeq@drawingapp.so9q8oz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
+    await listDatabases(client);
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+  } finally {
+    await client.close();
+    console.log("Closed connection to MongoDB");
+  }
+}
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+export default run;
