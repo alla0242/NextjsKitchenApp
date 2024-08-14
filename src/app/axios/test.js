625c53cdd42e2775b3e93472dbf74e215e60aa9a
mat <alla0242@algonquinlive.com>
2024-08-14 13:05:50 -0400

    changed uri in test

--- src/app/axios/test.js
+++ src/app/axios/test.js
@@ -2,7 +2,7 @@
 import { MongoClient } from "mongodb";
 
 async function run() {
-  const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
+  const uri = process.env.MONGODB_URI;
   const client = new MongoClient(uri);
 
   try {
