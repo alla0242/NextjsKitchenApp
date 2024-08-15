1e52847c53c6ca99aa1e6877667ecf2968e5820c
mat <alla0242@algonquinlive.com>
2024-08-15 11:47:32 -0400

    completed order function

--- src/app/FoH/completeOrder.js
+++ src/app/FoH/completeOrder.js
@@ -5,39 +5,65 @@ async function completeOrder(orderId, newState) {
   const uri = process.env.MONGODB_URI;
   const client = new MongoClient(uri);
   console.log("updateOrderState called with:", orderId, newState);
-  
+
   try {
     await client.connect();
     console.log("Connected to MongoDB");
-    
+
+    const timeTaken = await timeBetweenCreationAndCompletion(orderId);
+
     const updateStateToCompleted = await client
       .db("DrawingApp")
       .collection("images")
       .updateOne(
         { _id: new ObjectId(orderId) },
-        { $set: { state: newState, lastChangeTime: new Date(), lastChangeSource: "FoH", timeTaken: timeBetweenCreationAndCompletion } }
-        //Add time between creation time and completion time
+        {
+          $set: {
+            state: newState,
+            lastChangeTime: new Date(),
+            lastChangeSource: "FoH",
+            timeTaken: timeTaken,
+          },
+        }
       );
-        const getCompletedOrder = await client
+
+    const getCompletedOrder = await client
       .db("DrawingApp")
       .collection("images")
-      .findOne(
-        { _id: new ObjectId(orderId) })
+      .findOne({ _id: new ObjectId(orderId) });
+
+    await client
+      .db("DrawingApp")
+      .collection("pastOrders")
+      .insertOne(getCompletedOrder);
 
     await client
       .db("DrawingApp")
       .collection("images")
-      .insertOne(
-        getCompletedOrder
-      );
-   
-      
-    return result;
+      .deleteOne({ _id: new ObjectId(orderId) });
+
+    return updateStateToCompleted;
   } catch (error) {
     console.error("Error updating order state:", error);
   } finally {
     await client.close();
     console.log("MongoDB connection closed");
+  }
+}
+
+async function timeBetweenCreationAndCompletion(orderId) {
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const order = await client
+      .db("DrawingApp")
+      .collection("images")
+      .findOne({ _id: new ObjectId(orderId) });
+    return new Date().getTime() - order.timestamp.getTime();
+  } finally {
+    await client.close();
   }
 }
 
