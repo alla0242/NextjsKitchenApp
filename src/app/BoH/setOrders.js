30ad117fe4f5fbb0a03ad8b9653062d2e753d164
mat <alla0242@algonquinlive.com>
2024-08-15 09:10:42 -0400

    tesst

--- src/app/BoH/setOrders.js
+++ src/app/BoH/setOrders.js
@@ -4,7 +4,7 @@ import { MongoClient } from "mongodb";
 async function updateOrderState(orderId, newState) {
   const uri = process.env.MONGODB_URI;
   const client = new MongoClient(uri);
-
+  console.log(orderId, newState);
   try {
     await client.connect();
     await client
