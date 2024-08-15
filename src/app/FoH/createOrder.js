0a15471bcaea80eaf06e944f5675e0d176f086f8
mat <alla0242@algonquinlive.com>
2024-08-15 11:21:28 -0400

    try catch on sendToKitchen function

--- src/app/FoH/createOrder.js
+++ src/app/FoH/createOrder.js
@@ -1,21 +1,26 @@
 "use server"
 import { MongoClient } from "mongodb";
 
-
-
 export async function sendToKitchen(imageData) {
-      const uri = process.env.MONGODB_URI;
-      const client = new MongoClient(uri);
-      await client.connect();
-      const collection = client.db("DrawingApp").collection("images");
-      const order = {
-        imageData,
-        state: "Sent to Kitchen",
-        timestamp: new Date(),
-      };
-      await collection.insertOne(order);
-      await client.close();
-}
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri);
 
+  try {
+    await client.connect();
+    const collection = client.db("DrawingApp").collection("images");
+    const order = {
+      imageData,
+      state: "Sent to Kitchen",
+      timestamp: new Date(),
+    };
+    await collection.insertOne(order);
+    alert("Order sent to kitchen");
+  } catch (error) {
+    console.error("Error sending order to kitchen:", error);
+    alert("Failed to send order to kitchen");
+  } finally {
+    await client.close();
+  }
+}
 
 export default sendToKitchen;
