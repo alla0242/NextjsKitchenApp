18140620db67d121cf02f9cd81c0759e4b49baf5
mat <alla0242@algonquinlive.com>
2024-08-15 11:11:42 -0400

    send to kitchen test

--- src/app/FoH/createOrder.js
+++ src/app/FoH/createOrder.js
@@ -1,7 +1,19 @@
 "use server"
 
 
+
 export async function sendToKitchen(imageData) {
+      const uri = process.env.MONGODB_URI;
+      const client = new MongoClient(uri);
+      await client.connect();
+      const collection = client.db("DrawingApp").collection("test");
+      const order = {
+        imageData,
+        state: "Sent to Kitchen",
+        timestamp: new Date(),
+      };
+      await collection.insertOne(order);
+      await client.close();
     console.log(imageData);
 }
 
